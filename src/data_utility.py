119ea6041c110fa63398ef183f32be07f5277484
itsakcode <appsbyajay@gmail.com>
2024-02-25 19:54:32 -0500

    Add methods to encode, scale and process models

--- src/data_utility.py
+++ src/data_utility.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import OrdinalEncoder, LabelEncoder, StandardScaler
 
 def get_data_types(df):
     '''
@@ -38,9 +39,6 @@ def get_unique_values(df, category_columns):
         return col_unique_values
 
 def check_unique_values(df, cat_columns):
-    '''
-    
-    '''
     unique_values = {
         "col_name" : cat_columns,
         "num_unique_vals" : []
@@ -56,3 +54,73 @@ def check_unique_values(df, cat_columns):
 
     return None
 
+def get_ordinal_encoders(X_train, feature_details):
+    '''
+    
+    '''
+    encoders = {}
+
+    try:
+        # encode each column using Ordinal Encoder
+        for f_name, f_unique_dict in feature_details.items():
+            # get unique value names
+            category_values = list(f_unique_dict.keys())
+            ordinal_encoder = OrdinalEncoder(categories=[category_values], handle_unknown='use_encoded_value', unknown_value=-1)
+            encoders[f_name] = ordinal_encoder.fit(X_train[f_name].values.reshape(-1,1))
+
+    except Exception as ex:
+        print(f"get_ordinal_encoders:Error {ex}")
+
+    return encoders
+
+def encode_features(data, feature_encoders):
+    '''
+    
+    '''
+    data_encoded = data.copy()
+
+    try:
+        # encode each column using Ordinal Encoder
+        for f_name, f_encoder in feature_encoders.items():
+            data_encoded[f_name] = f_encoder.transform(data[f_name].values.reshape(-1, 1))
+        
+        return data_encoded
+    except Exception as ex:
+        print(f"encode_features:Error {ex}")
+
+def get_features_scaler(X_train, scaler=None):
+    '''
+    
+    '''
+    # use Standard Scaler by default
+    if scaler == None:
+        scaler = StandardScaler()
+
+    try: 
+        fitted_scaler = scaler.fit(X_train)
+        return fitted_scaler
+    
+    except Exception as ex:
+        print(f"scale_X_data:Error {ex}")
+
+def scale_features_data(X_data, scaler):
+    '''
+    
+    '''
+    try: 
+        X_data_scaled = scaler.transform(X_data)
+
+        return X_data_scaled
+    except Exception as ex:
+        print(f"scale_X_data:Error {ex}")
+
+def get_target_encoder(y_train):
+    '''
+    
+    '''
+    # encode target column using Label Encoder
+    label_encoder = LabelEncoder()
+
+    y_encoder = label_encoder.fit(y_train)
+
+    return y_encoder
--- src/model_utility.py
+++ src/model_utility.py
@@ -1,11 +1,33 @@
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, balanced_accuracy_score
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from xgboost import XGBClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostRegressor, ExtraTreesRegressor, RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.svm import SVR
+
+regression_models = {"Linear Regression" : LinearRegression(), 
+          "KNeighbors Regressor" : KNeighborsRegressor(n_neighbors=9), 
+          "RandomForest Regressor" : RandomForestRegressor(n_estimators=128, random_state=1), 
+          "ExtraTrees Regressor" : ExtraTreesRegressor(n_estimators=128, random_state=1), 
+          "AdaBoost Regressor" : AdaBoostRegressor(n_estimators=128, random_state=1), 
+          "SVR" : SVR(C=1.0, epsilon=0.2)
+}
+
+classification_models = {
+          "Logistic Regression" : LogisticRegression(random_state=42), 
+          "SVC" : SVC(kernel='poly', probability=True), 
+          "KNeighbors Classifier" : KNeighborsClassifier(n_neighbors=9), 
+          "DecisionTree Classifier" : DecisionTreeClassifier(), 
+          "RandomForest Classifier" : RandomForestClassifier(n_estimators=256, random_state=42),
+          'XGBoost Classifier' : XGBClassifier()
+}
 
 def prepare_train_test_data(X, y, scaler=None):
     '''
@@ -80,6 +102,61 @@ def process_models(models, X, y, scaler=None):
             }
         except Exception as ex: 
             print(f"process_models:Error: {ex}")
+    
+    return model_results
+
+
+def process_models(models, X_train, X_test, y_train, y_test):
+    '''
+        Process the models by preparing training and testing data, 
+        calculate accuracy score, confusion matrix and classification report
+
+        Arguments: models - is a dictionary with model name as key and model
+                    instance as value.
+                    X , y - data
+                    scaler - optional - if not passed MinMaxScaler will be used
+        
+        Returns: dictionary with model name as key and dictionary of 
+                all the metrics as value
+
+        Error: Errors will be caught and printed
+    '''
+    if models == None:
+        models = { 
+            'Logistic Regression' : LogisticRegression(max_iter=250, random_state=42),
+            'RandomForest Classifier' : RandomForestClassifier(n_estimators=500, random_state=42),
+            'SVC' : SVC(),
+            'XGBoost Classifier' : XGBClassifier()
+        }
+
+    model_results = {}
+
+    for m_name, model in models.items():
+        try:
+            model.fit(X_train, y_train)
+
+            t_score = model.score(X_train, y_train)
+
+            y_predict = model.predict(X_test)
+            
+            a_score = accuracy_score(y_test, y_predict)
+
+            bal_a_score = balanced_accuracy_score(y_test, y_predict)
+        
+            conf_matrix = confusion_matrix(y_test, y_predict)
+
+            class_report = classification_report(y_test, y_predict)
+
+            model_results[m_name] = {
+                "model" : model,
+                "train_score" : t_score,
+                "accuracy_score" : a_score,
+                "balanced_accuracy_score" : bal_a_score,
+                "confusion_matrix" : conf_matrix,
+                "classification_report" : class_report
+            }
+        except Exception as ex: 
+            print(f"process_models:{m_name}:Error: {ex}")
     
     return model_results
     
